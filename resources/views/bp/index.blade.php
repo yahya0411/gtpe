b9dba3ebc921c7a0a55b4dabeb3d742e87be3bd6
yahya0411 <mekkiy30@gmail.com>
2023-02-02 14:25:21 +0100

    Modify datatable

---
+++ resources/views/bp/index.blade.php
@@ -0,0 +1,136 @@
+@extends("layouts.master")
+
+@section("content")
+ <section class="content">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+
+                <div class="card">
+                    <div class="card-header">
+                      <h3 class="card-title"><i class="fa fa-building"></i> Les Bureaux de  P oste</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                      <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                        <tr>
+                          <th>Dénomination</th>
+                          <th>ID Marchand</th>
+                          <th>ID Terminal</th>
+                          <th>N° SERIE PUCE GSM</th>
+                          <th>Adresse IP</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        <tr>
+                          <td>Presto</td>
+                          <td>Opera 8.5</td>
+                          <td>Win 95+ / OSX.2+</td>
+                          <td>-</td>
+                          <td>A</td>
+                        </tr>
+                        <tr>
+                          <td>Presto</td>
+                          <td>Opera 9.0</td>
+                          <td>Win 95+ / OSX.3+</td>
+                          <td>-</td>
+                          <td>A</td>
+                        </tr>
+                        <tr>
+                          <td>Presto</td>
+                          <td>Opera 9.2</td>
+                          <td>Win 88+ / OSX.3+</td>
+                          <td>-</td>
+                          <td>A</td>
+                        </tr>
+                        <tr>
+                          <td>Presto</td>
+                          <td>Opera 9.5</td>
+                          <td>Win 88+ / OSX.3+</td>
+                          <td>-</td>
+                          <td>A</td>
+                        </tr>
+                        <tr>
+                          <td>Presto</td>
+                          <td>Opera for Wii</td>
+                          <td>Wii</td>
+                          <td>-</td>
+                          <td>A</td>
+                        </tr>
+                        <tr>
+                          <td>Presto</td>
+                          <td>Nokia N800</td>
+                          <td>N800</td>
+                          <td>-</td>
+                          <td>A</td>
+                        </tr>
+                        <tr>
+                          <td>Presto</td>
+                          <td>Nintendo DS browser</td>
+                          <td>Nintendo DS</td>
+                          <td>8.5</td>
+                          <td>C/A<sup>1</sup></td>
+                        </tr>
+                        <tr>
+                          <td>KHTML</td>
+                          <td>Konqureror 3.1</td>
+                          <td>KDE 3.1</td>
+                          <td>3.1</td>
+                          <td>C</td>
+                        </tr>
+                        <tr>
+                          <td>KHTML</td>
+                          <td>Konqureror 3.3</td>
+                          <td>KDE 3.3</td>
+                          <td>3.3</td>
+                          <td>A</td>
+                        </tr>
+
+                        <tr>
+                          <td>Misc</td>
+                          <td>NetFront 3.4</td>
+                          <td>Embedded devices</td>
+                          <td>-</td>
+                          <td>A</td>
+                        </tr>
+                        <tr>
+                          <td>Misc</td>
+                          <td>Dillo 0.8</td>
+                          <td>Embedded devices</td>
+                          <td>-</td>
+                          <td>X</td>
+                        </tr>
+                        <tr>
+                          <td>Misc</td>
+                          <td>Links</td>
+                          <td>Text only</td>
+                          <td>-</td>
+                          <td>X</td>
+                        </tr>
+                        <tr>
+                          <td>Misc</td>
+                          <td>Lynx</td>
+                          <td>Text only</td>
+                          <td>-</td>
+                          <td>X</td>
+                        </tr>
+
+                        </tbody>
+                        <tfoot>
+                        </tfoot>
+                      </table>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+
+            </div>
+        </div>
+    </div>
+ </section>
+
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,11 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+   <!-- data table -->
+   <link rel="stylesheet" href="{{asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+   <link rel="stylesheet" href="{{asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+   <link rel="stylesheet" href="{{asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
@@ -349,6 +354,19 @@
 </script>
 <!-- Bootstrap 4 -->
 <script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+<script src="{{asset('adminlte/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
 <!-- ChartJS -->
 <script src="{{asset('adminlte/plugins/chart.js/Chart.min.js')}}"></script>
 <!-- Sparkline -->
@@ -375,6 +393,23 @@
 <script src="{{asset('adminlte/dist/js/pages/dashboard.js')}}"></script>
 <script>
     $.widget.bridge('uibutton', $.ui.button)
+</script>
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
 </script>
 </body>
 </html>
